b4dff4e26d40f2a473e09fa08cbe6d83b3d29759
Devtrybe <63015481+dimagela29@users.noreply.github.com>
2020-08-17 15:28:05 -0300

    Add files via upload

---
+++ Aula 4 POO.py
@@ -0,0 +1,25 @@
+"""
+Tipos de dados
+str - string - textos 'Assim, "Assim"
+int - inteiro - 123456 0-10-20-50-60-15000-1500
+float real/pontoflutuante - 10.50 1.5 10.10 50.93 0.0
+bool - booleano/logico - true/false 10==10
+
+print('luiz', type('luiz'))
+print(10, type(10))
+print(0.0, typpe(0.0))
+print('l' == 'l', Type('l' == 'l'))
+"""
+
+#string: nome
+print('Demetrius', type('Demetrius'))
+
+#idade: int
+print('30', type('30'))
+
+#altura: float
+print('1.65', type('1.65'))
+
+#é maior de idade 10 > 20
+print(30 > 18, type(30 > 18))
+
