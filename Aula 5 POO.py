06a68ba110d34e888615002d59f8bf706e235ecd
Devtrybe <63015481+dimagela29@users.noreply.github.com>
2020-08-17 15:44:16 -0300

    Add files via upload

---
+++ Aula 5 POO.py
@@ -0,0 +1,21 @@
+"""
++ Adição
+- subtração
+* multiplicação
+/ divisão
+// divisão de numero inteiro
+** exponeciação
+% resto da divisão
+()Alterar precedencia
+"""
+
+print(10 * 10) # multiplicação
+print(10 + 10) #adição
+print(10 - 5) # subtração
+print(10 / 2) #divisão
+
+
+print(10 * 'k')
+
+print(10//3)
+
