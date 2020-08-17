ea5dc702344430e8b9c5a02d5eef98319e7418e5
Devtrybe <63015481+dimagela29@users.noreply.github.com>
2020-08-17 14:15:25 -0300

    Add files via upload

---
+++ Aula 2 POO.py
@@ -0,0 +1,9 @@
+#print(123456)
+print('Demetrius', 'Magela da' 'Mata', sep=' ', end='')
+print('Socio devtrybe', sep='-', end='')
+
+"""
+824.176.070-18
+"""
+print('824', '176', '070', sep='.', end='-')
+print('18')
