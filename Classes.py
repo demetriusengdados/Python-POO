5ef60566a3405cca7f2c2af548b51a26134de982
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-14 14:41:25 -0300

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,23 @@
+class Pessoa:
+  ano_atual = 2020
+  
+  def __init__(self, nome, idade):
+    self.nome = nome
+    self.idade = idade
+    
+    
+  def get_ano_nascimento(self):
+    print(self.ano_atual - self.idade)
+    
+    
+  @classmethod
+  def por_ano_nascimento(cls, nome, ano_nascimento):
+    idade = cls.ano_atual - ano_nascimento
+    return cls(nome, idade)
+  
+
+#p1 = Pessoa.por_ano_nascimento('Demetrius', 1990)
+p1 = Pessoa('Demetrius', 30)
+print(p1)
+print(p1.nome, p1.idade)
+p1.get_ano_nascimento()
