07a1cb90a645dbe11d5b08ca51c3b77ee3107fab
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-15 14:08:58 -0300

    Add files via upload

---
+++ Herança simples.py
@@ -0,0 +1,11 @@
+class Cliente:
+  def __init__(self, nome, idade):
+    self.nome = nome
+    self.idade = idade
+
+class Cliente(Pessoa):
+  pass
+
+
+class Aluno(Pessoa):
+  pass
---
+++ Main Herança simples.py
@@ -0,0 +1,10 @@
+from classes import *
+
+"""
+Associação - Usa \ Agregação - tem \ Composição - É dono \ Herança - É
+"""
+
+c1 = Cliente('Luiz', 45)
+print(c1.nome)
+a1 = Aluno('Maria', 54)
+print(a1.nome)
