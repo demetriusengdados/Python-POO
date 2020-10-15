20989654718772f166ab64a01c3f19b7349b277a
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-15 14:11:52 -0300

    Add files via upload

--- Herança simples.py
+++ Herança simples.py
@@ -2,10 +2,17 @@ class Cliente:
   def __init__(self, nome, idade):
     self.nome = nome
     self.idade = idade
+    self.nomeclasse = self.__class__.__name__
+    
+    
+  def falar(self):
+    print(f'{self.nomeclasse} falando...')
 
 class Cliente(Pessoa):
-  pass
+  def comprar(self):
+    print(f'{self.nomeclasse} comprando...')
 
 
 class Aluno(Pessoa):
-  pass
+  def estudar(self):
+    print(f'{self.nomeclasse} estudando...')
--- Main Herança simples.py
+++ Main Herança simples.py
@@ -5,6 +5,11 @@ Associação - Usa \ Agregação - tem \ Composição - É dono \ Herança - É
 """
 
 c1 = Cliente('Luiz', 45)
+c1.falar()
+c1.comprar()
 print(c1.nome)
+
 a1 = Aluno('Maria', 54)
+a1.falar()
+a1.estudar()
 print(a1.nome)
