4e3d8413149486c831623e2aafe6a18bdb9886b1
Demetrius <63015481+dimagela29@users.noreply.github.com>
2020-10-20 14:07:41 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -3,41 +3,42 @@ from abc import ABC, abstractmethod
 
 class Conta(ABC):
     def __init__(self, agencia, conta, saldo):
-        self._agencia = agencia
-        self._conta = conta
-        self._saldo = saldo
-
-    @property
-    def agencia(self):
-        return self._agencia
-
-    @property
-    def conta(self):
-        return self._conta
-
-    @property
-    def saldo(self):
-        return self._saldo
-
-    @saldo.setter
-    def saldo(self, valor):
-        if not isinstance(valor, (int, float)):
-            raise ValueError("Saldo precisa ser numérico")
-
-        self._saldo = valor
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
 
     def depositar(self, valor):
-        if not isinstance(valor, (int, float)):
-            raise ValueError("Valor do depósito precisa ser numérico")
-
         self.saldo += valor
         self.detalhes()
 
     def detalhes(self):
-        print(f'Agência: {self.agencia}', end=' ')
-        print(f'Conta: {self.conta}', end=' ')
-        print(f'Saldo: {self.saldo}')
+        print(f'Agência: {self.agencia} '
+              f'Conta: {self.conta} '
+              f'Saldo: {self.saldo}')
 
     @abstractmethod
+    def sacar(self, valor): pass
+
+
+class ContaPoupanca(Conta):
     def sacar(self, valor):
-        pass
+        if self.saldo < valor:
+            print('Saldo insuficiente')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo, limite=100):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        if (self.saldo + self.limite) < valor:
+            print('Saldo insuficiente')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
