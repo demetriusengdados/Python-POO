0aea0e83de87157a72693889c48590a58f1e1a95
Demetrius <63015481+dimagela29@users.noreply.github.com>
2021-01-31 10:29:59 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,49 @@
-"""
-Exercício com Abstração, Herança, Encapsulamento e Polimorfismo
-Criar um sistema bancário (extremamente simples) que tem clientes, contas e
-um banco. A ideia é que o cliente tenha uma conta (poupança ou corrente) e que
-possa sacar/depositar nessa conta. Contas corrente tem um limite extra. Banco
-tem clientes e contas.
+import sys
+from design import *
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5.QtGui import QPixmap
 
-Dicas:
-Criar classe Cliente que herda da classe Pessoa (Herança)
-    Pessoa tem nome e idade (com getters)
-    Cliente TEM conta (Agregação da classe ContaCorrente ou ContaPoupanca)
-Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
-    ContaCorrente deve ter um limite extra
-    Contas têm agência, número da conta e saldo
-    Contas devem ter método para depósito
-    Conta (super classe) deve ter o método sacar abstrato (Abstração e
-    polimorfismo - as subclasses que implementam o método sacar)
-Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
-Banco será responsável autenticar o cliente e as contas da seguinte maneira:
-    Banco tem contas e clentes (Agregação)
-    * Checar se a agência é daquele banco
-    * Checar se o cliente é daquele banco
-    * Checar se a conta é daquele banco
-Só será possível sacar se passar na autenticação do banco (descrita acima)
-"""
-from banco import Banco
-from cliente import Cliente
-from conta import ContaCorrente, ContaPoupanca
 
-banco = Banco()
+class Novo(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+        self.btnEscolherArquivo.clicked.connect(self.abrir_imagem)
+        self.btnRedimensionar.clicked.connect(self.redimensionar)
+        self.btnSalvar.clicked.connect(self.salvar)
 
-cliente1 = Cliente('Luiz', 30)
-cliente2 = Cliente('Maria', 18)
-cliente3 = Cliente('João', 50)
+    def abrir_imagem(self):
+        imagem, _ = QFileDialog.getOpenFileName(
+            self.centralwidget,
+            'Abrir imagem',
+            r'/home/luizotavio/Imagens/',
+            options=QFileDialog.DontUseNativeDialog
+        )
+        self.inputAbrirArquivo.setText(imagem)
+        self.original_img = QPixmap(imagem)
+        self.labelImg.setPixmap(self.original_img)
+        self.inputLargura.setText(str(self.original_img.width()))
+        self.inputAltura.setText(str(self.original_img.height()))
 
-conta1 = ContaPoupanca(1111, 254136, 0)
-conta2 = ContaCorrente(2222, 254137, 0)
-conta3 = ContaPoupanca(1212, 254138, 0)
+    def redimensionar(self):
+        largura = int(self.inputLargura.text())
+        self.nova_imagem = self.original_img.scaledToWidth(largura)
+        self.labelImg.setPixmap(self.nova_imagem)
+        self.inputLargura.setText(str(self.nova_imagem.width()))
+        self.inputAltura.setText(str(self.nova_imagem.height()))
 
-cliente1.inserir_conta(conta1)
-cliente2.inserir_conta(conta2)
-cliente3.inserir_conta(conta3)
+    def salvar(self):
+        imagem, _ = QFileDialog.getSaveFileName(
+            self.centralwidget,
+            'Salvar imagem',
+            r'/home/luizotavio/Desktop/',
+            options=QFileDialog.DontUseNativeDialog
+        )
+        self.nova_imagem.save(imagem, 'PNG')
 
-banco.inserir_cliente(cliente1)
-banco.inserir_conta(conta1)
 
-banco.inserir_cliente(cliente2)
-banco.inserir_conta(conta2)
-
-if banco.autenticar(cliente1):
-    cliente1.conta.depositar(0)
-    cliente1.conta.sacar(20)
-else:
-    print('Cliente não autenticado.')
-
-print('####################')
-
-if banco.autenticar(cliente2):
-    cliente2.conta.depositar(0)
-    cliente2.conta.sacar(20)
-else:
-    print('Cliente não autenticado.')
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    novo = Novo()
+    novo.show()
+    qt.exec_()
