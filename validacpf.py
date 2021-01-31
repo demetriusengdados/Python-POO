8b46a66df02fc99c5272ee8c56c31abd4ff60ed0
Demetrius <63015481+dimagela29@users.noreply.github.com>
2021-01-31 11:37:21 -0300

    Add files via upload

--- validacpf.py
+++ validacpf.py
@@ -1,26 +1,12 @@
-"""
-CPF = 168.995.350-09
-------------------------------------------------
-1 * 10 = 10           #    1 * 11 = 11 <-
-6 * 9  = 54           #    6 * 10 = 60
-8 * 8  = 64           #    8 *  9 = 72
-9 * 7  = 63           #    9 *  8 = 72
-9 * 6  = 54           #    9 *  7 = 63
-5 * 5  = 25           #    5 *  6 = 30
-3 * 4  = 12           #    3 *  5 = 15
-5 * 3  = 15           #    5 *  4 = 20
-0 * 2  = 0            #    0 *  3 = 0
-                      # -> 0 *  2 = 0
-         297          #             343
-11 - (297 % 11) = 11  #     11 - (343 % 11) = 9
-11 > 9 = 0            #
-Digito 1 = 0          #   Digito 2 = 9
-"""
+import re
 
-# Loop infinito
-while True:
-    # cpf = '16899535009'
-    cpf = input('Digite um CPF: ')
+def valida_cpf(cpf):
+    cpf = str(cpf)
+    cpf = re.sub(r'[^0-9]', '', cpf)
+
+    if not cpf or len(cpf) != 11:
+        return False
+
     novo_cpf = cpf[:-2]                 # Elimina os dois últimos digitos do CPF
     reverso = 10                        # Contador reverso
     total = 0
@@ -48,6 +34,6 @@ while True:
     # Descobri que sequências avaliavam como verdadeiro, então também
     # adicionei essa checagem aqui
     if cpf == novo_cpf and not sequencia:
-        print('Válido')
+        return True
     else:
-        print('Inválido')
+        return False
